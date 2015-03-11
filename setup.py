13b5ff70521e1288d88863fae29d7cf8048ddf2c
Yann Mahe <yann@datadoghq.com>
2015-03-11 12:00:38 -0400

    v0.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if [sys.version_info[0], sys.version_info[1]] < [2, 7]:
 
 setup(
     name="datadog",
-    version="0.1",
+    version="0.1.1",
     install_requires=install_reqs,
     tests_require=["tox"],
     packages=[
