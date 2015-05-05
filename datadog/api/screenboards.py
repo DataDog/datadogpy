920bc2620533cb4c91d7b7dd186ba59fd09edbf9
Matthieu Allegre <matthieu.allegre@datadoghq.com>
2015-05-05 19:58:23 +0000

    Add `revoke` method to Screenboard
    - This method allow to revoke the public access of a shared
    screenboard

--- datadog/api/screenboards.py
+++ datadog/api/screenboards.py
@@ -23,3 +23,15 @@ class Screenboard(GetableAPIResource, CreateableAPIResource,
         :returns: JSON response from HTTP request
         """
         return super(Screenboard, cls)._trigger_action('GET', 'screen/share', board_id)
+
+    @classmethod
+    def revoke(cls, board_id):
+        """
+        Revoke a shared screenboard with given id
+
+        :param board_id: screenboard to revoke
+        :type board_id: id
+
+        :returns: JSON response from HTTP request
+        """
+        return super(Screenboard, cls)._trigger_action('DELETE', 'screen/share', board_id)
