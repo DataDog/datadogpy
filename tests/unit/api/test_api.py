a017a2d3b7e59e31c60ff3c14170fe10f51c8f24
Alexey Kuzminich <akuzminich@gmail.com>
2015-06-02 16:45:08 +0800

    Some cosmetic changes to unit tests for Metric.send()

--- tests/unit/api/test_api.py
+++ tests/unit/api/test_api.py
@@ -206,8 +206,7 @@ class TestResources(DatadogAPIWithInitialization):
         self.request_called_with('GET', "host/api/v1/query",
                                  params={'from': "val1", 'to': "val2"})
 
-    def test_metric_submit_multiple(self):
-        """"""
+    def test_metric_submit_multiple_points_is_single_value(self):
         from copy import deepcopy
         import json
         from time import time
@@ -218,23 +217,22 @@ class TestResources(DatadogAPIWithInitialization):
                 dict(metric='metric.2', points=19)]
 
         Metric.send(*deepcopy(data))
-        #Metric.send(deepcopy(data))
 
         args, kwargs = self.request_mock.request.call_args
         sent_data = json.loads(kwargs['data'])
-        #print(args)
-        #print(kwargs)
-        #print(sent_data)
 
         for i, metric in enumerate(sent_data['series']):
-            print(metric)
             assert set(metric.keys()) == set(['metric', 'points', 'host'])
 
             assert metric['metric'] == data[i]['metric']
             assert metric['host'] == api._host_name
 
-            assert isinstance(metric['points'], list) and len(metric['points']) == 1 # there's array of 1 point
-            assert len(metric['points'][0]) == 2 # it consists of a [time, value] pair
-            assert metric['points'][0][1] == data[i]['points'] # its value == value we sent
-            assert now - 1 < metric['points'][0][0] < now + 1 # it's time not so far from current time
+            # points is a list of 1 point
+            assert isinstance(metric['points'], list) and len(metric['points']) == 1
+            # it consists of a [time, value] pair
+            assert len(metric['points'][0]) == 2
+            # its value == value we sent
+            assert metric['points'][0][1] == data[i]['points']
+            # it's time not so far from current time
+            assert now - 1 < metric['points'][0][0] < now + 1
 
