c8a6bfbe21149de53b494b6ee736156ed45f4d1e
Yann MAHE <yann@datadoghq.com>
2015-06-24 22:29:54 -0400

    v0.7.0
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if [sys.version_info[0], sys.version_info[1]] < [2, 7]:
 
 setup(
     name="datadog",
-    version="0.6.0",
+    version="0.7.0",
     install_requires=install_reqs,
     tests_require=["tox", "nose", "mock", "six", "pillow"],
     packages=[
