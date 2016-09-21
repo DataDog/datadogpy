84d0796e011d4a8f19a503488890f56365b95abd
Yann MAHE <yann@datadoghq.com>
2016-09-21 12:50:37 -0400

    [dogstatsd] organize coroutine related methods 📚
    * Move Python 3.5+ evaluation methods to `datadog.util.compat` module
    * Move `TimedContextManager` to `datadog.dogstatsd.context` module
    * Quarantine `TimedContextManager` coroutine decorator in
      `datadog.dogstatsd.context_async`
    * Unit test coverage to assert DogStatsd compatibility with coroutine
      functions

--- datadog/dogstatsd/context_async.py
+++ datadog/dogstatsd/context_async.py
@@ -1,8 +1,17 @@
+"""
+Decorator `timed` for coroutine methods.
+
+Warning: requires Python 3.5 or higher.
+"""
+# stdlib
 from functools import wraps
 from time import time
 
 
 def _get_wrapped_co(self, func):
+    """
+    `timed` wrapper for coroutine methods.
+    """
     @wraps(func)
     async def wrapped_co(*args, **kwargs):
         start = time()
