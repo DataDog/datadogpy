ceaee34efcdd8878dd8f6a9aaa17fa7fbbad1dcc
Ofek Lev <ofekmeister@gmail.com>
2019-01-15 12:49:21 -0500

    Support Python 3.7, drop 3.3

--- setup.py
+++ setup.py
@@ -49,10 +49,10 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: PyPy",
     ]
 )
