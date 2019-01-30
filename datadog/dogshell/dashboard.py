53ea14e000e7d00093165dead4ee4ad3f27c2655
Rami Enbashi <rami.enbashi@datadoghq.com>
2019-01-30 16:24:29 -0500

    [dashboards] Review feedback changes

--- datadog/dogshell/dashboard.py
+++ datadog/dogshell/dashboard.py
@@ -24,38 +24,53 @@ class DashboardClient(object):
         verb_parsers.required = True
 
         post_parser = verb_parsers.add_parser('post', help="Create dashboards")
+        # Required arguments:
         post_parser.add_argument('title', help="title for the new dashboard")
-        post_parser.add_argument('description', help="short description of the dashboard")
-        post_parser.add_argument('widgets', help="widget definitions as a JSON string. if unset,"
+        post_parser.add_argument('widgets', help="widget definitions as a JSON string. If unset,"
                                  " reads from stdin.", nargs="?")
-        post_parser.add_argument('--template_variables', type=_template_variables, default=[],
-                                 help="a json list of template variable dicts, e.g. "
-                                 "[{'name': 'host', 'prefix': 'host', "
-                                 "'default': 'host:my-host'}]\'")
-
+        # Optional arguments:
+        post_parser.add_argument('--description', help="Short description of the dashboard")
+        post_parser.add_argument('--read_only', help="Whether this dashboard is read-only. "
+                                 "If True, only the author and admins can make changes to it.",
+                                 action='store_true')
+        post_parser.add_argument('--notify_list', type=_json_string,
+                                 help="A json list of user handles, e.g. "
+                                 "'[\"user1@domain.com\", \"user2@domain.com\"]'")
+        post_parser.add_argument('--template_variables', type=_json_string,
+                                 help="A json list of template variable dicts, e.g. "
+                                 "'[{\"name\": \"host\", \"prefix\": \"host\", "
+                                 "\"default\": \"my-host\"}]'")
         post_parser.set_defaults(func=cls._post)
 
         update_parser = verb_parsers.add_parser('update', help="Update existing dashboards")
-        update_parser.add_argument('dashboard_id', help="dashboard to replace"
+        # Required arguments:
+        update_parser.add_argument('dashboard_id', help="Dashboard to replace"
                                    " with the new definition")
-        update_parser.add_argument('title', help="new title for the dashboard")
-        update_parser.add_argument('description', help="short description of the dashboard")
-        update_parser.add_argument('widgets', help="widget definitions as a JSON string."
-                                   " if unset, reads from stdin", nargs="?")
-        update_parser.add_argument('--template_variables', type=_template_variables, default=[],
-                                   help="a json list of template variable dicts, e.g. "
-                                   "[{'name': 'host', 'prefix': 'host', "
-                                   "'default': 'host:my-host'}]\'")
+        update_parser.add_argument('title', help="New title for the dashboard")
+        update_parser.add_argument('widgets', help="Widget definitions as a JSON string."
+                                   " If unset, reads from stdin", nargs="?")
+        # Optional arguments:
+        update_parser.add_argument('--description', help="Short description of the dashboard")
+        update_parser.add_argument('--read_only', help="Whether this dashboard is read-only. "
+                                   "If True, only the author and admins can make changes to it.",
+                                   action='store_true')
+        update_parser.add_argument('--notify_list', type=_json_string,
+                                   help="A json list of user handles, e.g. "
+                                   "'[\"user1@domain.com\", \"user2@domain.com\"]'")
+        update_parser.add_argument('--template_variables', type=_json_string,
+                                   help="A json list of template variable dicts, e.g. "
+                                   "'[{\"name\": \"host\", \"prefix\": \"host\", "
+                                   "\"default\": \"my-host\"}]'")
         update_parser.set_defaults(func=cls._update)
 
         show_parser = verb_parsers.add_parser('show', help="Show a dashboard definition")
-        show_parser.add_argument('dashboard_id', help="dashboard to show")
+        show_parser.add_argument('dashboard_id', help="Dashboard to show")
         show_parser.set_defaults(func=cls._show)
 
         pull_parser = verb_parsers.add_parser('pull', help="Pull a dashboard on the server"
                                               " into a local file")
         pull_parser.add_argument('dashboard_id', help="ID of dashboard to pull")
-        pull_parser.add_argument('filename', help="file to pull dashboard into")
+        pull_parser.add_argument('filename', help="File to pull dashboard into")
         pull_parser.set_defaults(func=cls._pull)
 
         push_parser = verb_parsers.add_parser('push', help="Push updates to dashboards"
@@ -63,24 +78,24 @@ class DashboardClient(object):
         push_parser.add_argument('--append_auto_text', action='store_true', dest='append_auto_text',
                                  help="When pushing to the server, appends filename"
                                  " and timestamp to the end of the dashboard description")
-        push_parser.add_argument('file', help="dashboard files to push to the server",
+        push_parser.add_argument('file', help="Dashboard files to push to the server",
                                  nargs='+', type=argparse.FileType('r'))
         push_parser.set_defaults(func=cls._push)
 
         new_file_parser = verb_parsers.add_parser('new_file', help="Create a new dashboard"
                                                   " and put its contents in a file")
-        new_file_parser.add_argument('filename', help="name of file to create with empty dashboard")
-        new_file_parser.add_argument('widgets', help="widget definitions as a JSON string."
-                                     " if unset, reads from stdin.", nargs="?")
+        new_file_parser.add_argument('filename', help="Name of file to create with empty dashboard")
+        new_file_parser.add_argument('widgets', help="Widget definitions as a JSON string."
+                                     " If unset, reads from stdin.", nargs="?")
         new_file_parser.set_defaults(func=cls._new_file)
 
         web_view_parser = verb_parsers.add_parser('web_view',
                                                   help="View the dashboard in a web browser")
-        web_view_parser.add_argument('file', help="dashboard file", type=argparse.FileType('r'))
+        web_view_parser.add_argument('file', help="Dashboard file", type=argparse.FileType('r'))
         web_view_parser.set_defaults(func=cls._web_view)
 
         delete_parser = verb_parsers.add_parser('delete', help="Delete dashboards")
-        delete_parser.add_argument('dashboard_id', help="dashboard to delete")
+        delete_parser.add_argument('dashboard_id', help="Dashboard to delete")
         delete_parser.set_defaults(func=cls._delete)
 
     @classmethod
@@ -134,35 +149,38 @@ class DashboardClient(object):
         api._timeout = args.timeout
         for f in args.file:
             try:
-                dash_obj = json.load(f)
+                payload = json.load(f)
             except Exception as err:
                 raise Exception("Could not parse {0}: {1}".format(f.name, err))
 
             if args.append_auto_text:
-                datetime_str = datetime.now().strftime('%x %X')
-                auto_text = ("<br/>\nUpdated at {0} from {1} ({2}) on {3}"
-                             .format(datetime_str, f.name, dash_obj["id"], platform.node()))
-                dash_obj["description"] += auto_text
-            tpl_vars = dash_obj.get("template_variables", [])
+                if 'description' not in payload or payload['description'] is None:
+                    payload["description"] = ""
 
-            if 'id' in dash_obj:
+                datetime_str = datetime.now().strftime('%x %X')
+                auto_text = ("\nUpdated at {0} from {1} ({2}) on {3}"
+                             .format(datetime_str, f.name, payload["id"], platform.node()))
+                payload["description"] += auto_text
+            tpl_vars = payload.get("template_variables", [])
+
+            if 'id' in payload:
                 res = api.Dashboard.update(
-                    dash_obj["id"],
-                    title=dash_obj["title"],
-                    description=dash_obj["description"],
-                    widgets=dash_obj["widgets"],
+                    payload["id"],
+                    title=payload["title"],
+                    description=payload["description"],
+                    widgets=payload["widgets"],
                     layout_type="ordered",
                     template_variables=tpl_vars)
             else:
                 res = api.Dashboard.create(
-                    title=dash_obj["title"],
-                    description=dash_obj["description"],
-                    widgets=dash_obj["widgets"],
+                    title=payload["title"],
+                    description=payload["description"],
+                    widgets=payload["widgets"],
                     layout_type="ordered",
                     template_variables=tpl_vars)
             if 'errors' in res:
                 print_err('Upload of dashboard {0} from file {1} failed.'
-                          .format(dash_obj["id"], f.name))
+                          .format(payload["id"], f.name))
 
             report_warnings(res)
             report_errors(res)
@@ -173,7 +191,7 @@ class DashboardClient(object):
                 print(json.dumps(res))
 
             if args.format == 'pretty':
-                print("Uploaded file {0} (dashboard {1})".format(f.name, dash_obj["id"]))
+                print("Uploaded file {0} (dashboard {1})".format(f.name, payload["id"]))
 
     @classmethod
     def _post(cls, args):
@@ -186,12 +204,24 @@ class DashboardClient(object):
             widgets = json.loads(widgets)
         except:
             raise Exception('bad json parameter')
-        res = api.Dashboard.create(
-            title=args.title,
-            description=args.description,
-            widgets=widgets,
-            layout_type="ordered",
-            template_variables=args.template_variables)
+
+        # Required arguments
+        payload = {
+            "title": args.title,
+            "widgets": widgets,
+            "layout_type": "ordered"
+        }
+        # Optional arguments
+        if(args.description):
+            payload["description"] = args.description
+        if(args.read_only):
+            payload["is_read_only"] = args.read_only
+        if(args.notify_list):
+            payload["notify_list"] = args.notify_list
+        if(args.template_variables):
+            payload["template_variables"] = args.template_variables
+
+        res = api.Dashboard.create(**payload)
         report_warnings(res)
         report_errors(res)
         if format == 'pretty':
@@ -211,13 +241,24 @@ class DashboardClient(object):
         except:
             raise Exception('bad json parameter')
 
+        # Required arguments
+        payload = {
+            "title": args.title,
+            "widgets": widgets,
+            "layout_type": "ordered"
+        }
+        # Optional arguments
+        if(args.description):
+            payload["description"] = args.description
+        if(args.read_only):
+            payload["is_read_only"] = args.read_only
+        if(args.notify_list):
+            payload["notify_list"] = args.notify_list
+        if(args.template_variables):
+            payload["template_variables"] = args.template_variables
+
         res = api.Dashboard.update(
-            args.dashboard_id,
-            title=args.title,
-            description=args.description,
-            widgets=widgets,
-            layout_type="ordered",
-            template_variables=args.template_variables)
+            args.dashboard_id, **payload)
         report_warnings(res)
         report_errors(res)
         if format == 'pretty':
@@ -253,16 +294,9 @@ class DashboardClient(object):
         url = api._api_host + "/dashboard/{0}".format(dash_id)
         webbrowser.open(url)
 
-    @classmethod
-    def _escape(cls, s):
-        return s.replace("\r", "\\r").replace("\n", "\\n").replace("\t", "\\t")
 
-
-def _template_variables(tpl_var_input):
-    if '[' not in tpl_var_input:
-        return [v.strip() for v in tpl_var_input.split(',')]
-    else:
-        try:
-            return json.loads(tpl_var_input)
-        except Exception:
-            raise argparse.ArgumentTypeError('bad template_variable json parameter')
+def _json_string(str):
+    try:
+        return json.loads(str)
+    except Exception:
+        raise argparse.ArgumentTypeError('bad json parameter')
