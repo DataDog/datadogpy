a058b90e7bc0b89feb65cf541d4a83c09ebd8040
David Bouchare <david.bouchare@datadoghq.com>
2019-02-05 15:15:45 +0100

    Handle more socket exceptions (#349)
    * Handle all exceptions
    
    * Change log levels and increase max line for tests
    
    * Add lock to address threading issues
    
    * Remove conversion to str
    
    Co-Authored-By: dabcoder <david.bouchare@datadoghq.com>

--- datadog/dogstatsd/base.py
+++ datadog/dogstatsd/base.py
@@ -7,6 +7,7 @@ from random import random
 import logging
 import os
 import socket
+from threading import Lock
 
 # datadog
 from datadog.dogstatsd.context import TimedContextManagerDecorator
@@ -58,6 +59,8 @@ class DogStatsd(object):
         UDP. If set, disables UDP transmission (Linux only)
         :type socket_path: string
         """
+
+        self.lock = Lock()
 
         # Connection
         if socket_path is not None:
@@ -114,16 +117,17 @@ class DogStatsd(object):
         Note: connect the socket before assigning it to the class instance to
         avoid bad thread race conditions.
         """
-        if not self.socket:
-            if self.socket_path is not None:
-                sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-                sock.connect(self.socket_path)
-                sock.setblocking(0)
-                self.socket = sock
-            else:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                sock.connect((self.host, self.port))
-                self.socket = sock
+        with self.lock:
+            if not self.socket:
+                if self.socket_path is not None:
+                    sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+                    sock.connect(self.socket_path)
+                    sock.setblocking(0)
+                    self.socket = sock
+                else:
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    sock.connect((self.host, self.port))
+                    self.socket = sock
 
         return self.socket
 
@@ -289,9 +293,12 @@ class DogStatsd(object):
         except socket.timeout:
             # dogstatsd is overflowing, drop the packets (mimicks the UDP behaviour)
             return
-        except socket.error:
-            log.info("Error submitting packet, dropping the packet and closing the socket")
+        except (socket.error, socket.herror, socket.gaierror) as se:
+            log.warning("Error submitting packet: {}, dropping the packet and closing the socket".format(se))
             self.close_socket()
+        except Exception as e:
+            log.error("Unexpected error: %s", str(e))
+            return
 
     def _send_to_buffer(self, packet):
         self.buffer.append(packet)
