500e96369108eee6eef6378a64efbe2dab898f3b
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2019-05-29 15:09:07 +0200

    Import Iterable from collections.abc on python3 (#381)

--- datadog/api/format.py
+++ datadog/api/format.py
@@ -1,4 +1,7 @@
-from collections import Iterable
+try:
+    from collections.abc import Iterable
+except ImportError:
+    from collections import Iterable
 from numbers import Number
 import time
 
