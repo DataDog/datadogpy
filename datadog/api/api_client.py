38defcebfec9d4e110175ba345b725e671e3da3c
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-06-10 11:57:56 -0400

    Extend response headers to response

--- datadog/api/api_client.py
+++ datadog/api/api_client.py
@@ -158,7 +158,7 @@ class APIClient(object):
                 response_obj = None
 
             if response_obj:
-                response_obj['response_headers'] = response_headers
+                response_obj.append({'response_headers': response_headers})
 
             if response_formatter is None:
                 return response_obj
