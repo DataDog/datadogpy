a8708e5df3d0606397e8a52012d19fbcd09debf0
Julien Danjou <julien@danjou.info>
2019-11-19 16:35:32 -0500

    Revert "Convert setup.py to setup.cfg (#475)" (#484)
    This reverts commit 067106c2577d6e7952607d76419e4469a694e79f.

--- setup.py
+++ setup.py
@@ -1,3 +1,50 @@
 from setuptools import setup
 
-setup()
+from io import open
+import sys
+
+
+def get_readme_md_contents():
+    """read the contents of your README file"""
+    with open("README.md", encoding='utf-8') as f:
+        long_description = f.read()
+        return long_description
+
+
+install_reqs = ["decorator>=3.3.2", "requests>=2.6.0"]
+
+if [sys.version_info[0], sys.version_info[1]] < [2, 7]:
+    install_reqs.append("argparse>=1.2")
+
+setup(
+    name="datadog",
+    version="0.32.0",
+    install_requires=install_reqs,
+    tests_require=["pytest", "mock", "freezegun"],
+    packages=["datadog", "datadog.api", "datadog.dogstatsd", "datadog.threadstats", "datadog.util", "datadog.dogshell"],
+    author="Datadog, Inc.",
+    long_description=get_readme_md_contents(),
+    long_description_content_type="text/markdown",
+    author_email="dev@datadoghq.com",
+    description="The Datadog Python library",
+    license="BSD",
+    keywords="datadog",
+    url="https://www.datadoghq.com",
+    project_urls={
+        "Bug Tracker": "https://github.com/DataDog/datadogpy/issues",
+        "Documentation": "https://datadogpy.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/DataDog/datadogpy",
+    },
+    entry_points={"console_scripts": ["dog = datadog.dogshell:main", "dogwrap = datadog.dogshell.wrap:main"]},
+    test_suite="tests",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
