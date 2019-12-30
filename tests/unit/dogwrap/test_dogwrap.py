c614530d916038e470e10951a547b44209c2ed1b
Gregory <gregory.zussa@datadoghq.com>
2019-12-30 22:30:10 +0100

    encoding order

--- tests/unit/dogwrap/test_dogwrap.py
+++ tests/unit/dogwrap/test_dogwrap.py
@@ -1,7 +1,7 @@
+# coding: utf8
+
 # Unless explicitly stated otherwise all files in this repository are licensed under the BSD-3-Clause License.
 # This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2020 Datadog, Inc.
-
-# coding: utf8
 
 import unittest
 import os
