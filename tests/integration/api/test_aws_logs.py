eea592edd803fb033d087b17f11ee23a5af6d90a
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2020-01-06 14:32:59 +0100

    oops"

--- tests/integration/api/test_aws_logs.py
+++ tests/integration/api/test_aws_logs.py
@@ -1,6 +1,6 @@
 # Unless explicitly stated otherwise all files in this repository are licensed under the BSD-3-Clause License.
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
-Copyright 2015-Present Datadog, Inc
+# Copyright 2015-Present Datadog, Inc
 from datadog import api as dog
 from datadog import initialize
 from tests.integration.api.constants import API_KEY, APP_KEY, API_HOST
