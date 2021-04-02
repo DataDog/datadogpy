5c1f72dae10b84cb9d93e9e73ed2360f4b7bffbf
artem888881 <81821454+artem888881@users.noreply.github.com>
2021-04-02 18:54:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ install_reqs = [
 
 if sys.version_info < (2, 7):
     install_reqs.append("argparse>=1.2")
-    "decorator<5.0.0",
+    install_reqs.append("decorator<5.0.0")
 else:
-    "decorator>=3.3.2"
+    install_reqs.append("decorator>=3.3.2")
 
 setup(
     name="datadog",
