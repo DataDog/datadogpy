1a90bead0d07ad36c8e70be507ce26f966b9f10e
Srdjan Grubor <sgnn7@sgnn7.org>
2021-06-30 02:46:41 -0500

    [statsd] Improve tag normalization speed (#672)
    * [tests] Add ability to print profiling stats in statsd benchmark test
    
    This change allows us to exactly see where statsd is spending the
    majority of its time when processing metrics. The profiling can be
    activated via `BENCHMARK_PROFILING` variable.
    
    * [statsd] Improve performance of tag normalization
    
    Previously, we used a raw string in `re.sub` which is in the hot-path
    and needed compilation every time so now we use the precompiled version
    of the regex to do the substitution, yielding a much better performance.

--- tests/unit/util/test_format.py
+++ tests/unit/util/test_format.py
@@ -1,9 +1,12 @@
+# coding: utf8
 # Unless explicitly stated otherwise all files in this repository are licensed under the BSD-3-Clause License.
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2015-Present Datadog, Inc
+import unittest
+
 import pytest
 
-from datadog.util.format import construct_url
+from datadog.util.format import construct_url, normalize_tags
 
 
 class TestConstructURL:
@@ -30,3 +33,20 @@ class TestConstructURL:
     @pytest.mark.parametrize("host,api_version,path,expected", test_data)
     def test_construct_url(self, host, api_version, path, expected):
         assert construct_url(host, api_version, path) == expected
+
+class TestNormalizeTags:
+    """
+    Test of the format's `normalize_tags` functionality
+    """
+    test_data = [
+        (['this is a tag'], ['this_is_a_tag']),
+        (['abc!@#$%^&*()0987654321{}}{'], ['abc__________0987654321____']),
+        (['abc!@#', '^%$#3456#'], ['abc___', '____3456_']),
+        (['mutliple', 'tags', 'included'], ['mutliple', 'tags', 'included']),
+        ([u'абвгдежзийкл', u'абв' , 'test123'], [u'абвгдежзийкл', u'абв' , 'test123']),
+        ([u'абвгд西😃ежзийкл', u'аб😃西в' , u'a😃😃b'],  [u'абвгд西_ежзийкл', u'аб_西в', u'a__b']),
+    ]
+
+    @pytest.mark.parametrize("original_tags,expected_tags", test_data)
+    def test_normalize_tags(self, original_tags, expected_tags):
+            assert normalize_tags(original_tags) == expected_tags
