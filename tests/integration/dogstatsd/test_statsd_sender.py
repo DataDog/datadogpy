2dcf88dedf375f1df7aa3ea82aaf5691e9f219e5
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2023-08-01 14:54:46 +0000

    Add simple integration test for statsd

---
+++ tests/integration/dogstatsd/test_statsd_sender.py
@@ -0,0 +1,39 @@
+import itertools
+import socket
+from threading import Thread
+
+import pytest
+
+from datadog.dogstatsd.base import DogStatsd
+
+@pytest.mark.parametrize(
+    "disable_sender, disable_buffering, wait_for_pending, socket_timeout",
+    list(itertools.product(*[[True, False]]*3, [0, 1])),
+)
+def test_sender_mode(disable_sender, disable_buffering, wait_for_pending, socket_timeout):
+    # Test basic sender operation with an assortment of options
+    foo, bar = socket.socketpair(socket.AF_UNIX, socket.SOCK_DGRAM, 0)
+    statsd = DogStatsd(
+        telemetry_min_flush_interval=0,
+        disable_sender=disable_sender,
+        disable_buffering=disable_buffering,
+        socket_timeout=socket_timeout,
+    )
+
+    statsd.socket = foo
+    statsd._reset_telemetry()
+
+    def reader_thread():
+        msg = bar.recv(8192)
+        assert msg == b"test.metric:1|c\n"
+
+    t = Thread(target=reader_thread, name="test_sender_mode/reader_thread")
+    t.daemon = True
+    t.start()
+
+    statsd.increment("test.metric")
+    if wait_for_pending:
+        statsd.wait_for_pending()
+
+    t.join(timeout=10)
+    assert not t.is_alive()
