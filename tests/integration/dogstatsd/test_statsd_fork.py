b7cab0ab84d02b53bd95c6069c40fda3bafdfd50
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2024-06-24 13:37:29 +0000

    Fix a potential deadlock on fork
    If a fork happens while some thread is sending metrics and holds one
    of the locks, the lock would remain locked in the child process and
    would deadlock, either in the post_fork handler (when the post_fork
    hook tries to close the socket), or later (when user code tries to
    send a metric).
    
    Work around the issue by resetting the socket and buffer locks in the
    child process. If those were locked in the parent at the time of the
    fork, the internal client state may be inconsistent, so we reset it as
    well.
    
    With config lock, we can not reset the state to some known good state,
    and to avoid problems when fork is called while a thread modifies the
    client configuration, the config lock will be held across fork. Both
    the client and the parent can safely unlock it afterwards.

--- tests/integration/dogstatsd/test_statsd_fork.py
+++ tests/integration/dogstatsd/test_statsd_fork.py
@@ -1,6 +1,7 @@
 import os
 import itertools
 import socket
+import threading
 
 import pytest
 
@@ -31,7 +32,7 @@ def test_register_at_fork(disable_background_sender, disable_buffering):
         return inner
 
     statsd.pre_fork = track(statsd.pre_fork)
-    statsd.post_fork = track(statsd.post_fork)
+    statsd.post_fork_parent = track(statsd.post_fork_parent)
 
     pid = os.fork()
     if pid == 0:
@@ -41,3 +42,49 @@ def test_register_at_fork(disable_background_sender, disable_buffering):
     os.waitpid(pid, 0)
 
     assert len(tracker) == 2
+
+
+def sender_a(statsd, running):
+    while running[0]:
+        statsd.gauge("spam", 1)
+
+
+def sender_b(statsd, signal):
+    while running[0]:
+        with statsd:
+            statsd.gauge("spam", 1)
+
+@pytest.mark.parametrize(
+    "disable_background_sender, disable_buffering, sender",
+    list(itertools.product([True, False], [True, False], [sender_a, sender_b])),
+)
+def test_fork_with_thread(disable_background_sender, disable_buffering, sender):
+    if not SUPPORTS_FORKING:
+        pytest.skip("os.register_at_fork is required for this test")
+
+    statsd = DogStatsd(
+        telemetry_min_flush_interval=0,
+        disable_background_sender=disable_background_sender,
+        disable_buffering=disable_buffering,
+    )
+
+    sender = None
+    try:
+        sender_running = [True]
+        sender = threading.Thread(target=sender, args=(statsd, sender_running))
+        sender.daemon = True
+        sender.start()
+
+        pid = os.fork()
+        if pid == 0:
+            os._exit(42)
+
+        assert pid > 0
+        (_, status) = os.waitpid(pid, 0)
+
+        assert os.WEXITSTATUS(status) == 42
+    finally:
+        statsd.stop()
+        if sender:
+            sender_running[0] = False
+            sender.join()
