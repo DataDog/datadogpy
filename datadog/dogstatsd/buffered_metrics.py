6cf84bd375d59083d744ab80db5171d65a3f1edd
andrewqian2001datadog <andrew.qian@datadoghq.com>
2024-09-10 13:05:44 -0400

    rename variables/methods

--- datadog/dogstatsd/buffered_metrics.py
+++ datadog/dogstatsd/buffered_metrics.py
@@ -3,43 +3,43 @@ from datadog.dogstatsd.metric_types import MetricType
 
 
 class BufferedMetric(object):
-    def __init__(self, name, tags, metric_type, max_metrics=0, specified_rate=1.0):
+    def __init__(self, name, tags, metric_type, max_metric_samples=0, specified_rate=1.0):
         self.name = name
         self.tags = tags
         self.metric_type = metric_type
-        self.max_metrics = max_metrics
+        self.max_metric_samples = max_metric_samples
         self.specified_rate = specified_rate
         self.data = []
-        self.stored_metrics = 1
-        self.total_metrics = 1
+        self.stored_metric_samples = 1
+        self.total_metric_samples = 1
 
     def aggregate(self, value):
         self.data.append(value)
-        self.stored_metrics += 1
-        self.total_metrics += 1
+        self.stored_metric_samples += 1
+        self.total_metric_samples += 1
 
     def maybe_add_metric(self, value):
-        if self.max_metrics > 0:
-            if self.stored_metrics >= self.max_metrics:
-                i = random.randint(0, self.total_metrics - 1)
-                if i < self.max_metrics:
+        if self.max_metric_samples > 0:
+            if self.stored_metric_samples >= self.max_metric_samples:
+                i = random.randint(0, self.total_metric_samples - 1)
+                if i < self.max_metric_samples:
                     self.data[i] = value
             else:
                 self.data.append(value)
-                self.stored_metrics += 1
-            self.total_metrics += 1
+                self.stored_metric_samples += 1
+            self.total_metric_samples += 1
         else:
             self.aggregate(value)
 
     def skip_metric(self):
-        self.total_metrics += 1
+        self.total_metric_samples += 1
 
     def flush(self):
-        total_metrics = self.total_metrics
+        total_metric_samples = self.total_metric_samples
         if self.specified_rate != 1.0:
             rate = self.specified_rate
         else:
-            rate = self.stored_metrics / total_metrics
+            rate = self.stored_metric_samples / total_metric_samples
 
         return {
             'name': self.name,
@@ -51,18 +51,18 @@ class BufferedMetric(object):
 
 
 class HistogramMetric(BufferedMetric):
-    def __init__(self, name, value, tags, max_metrics=0, rate=1.0):
-        super(HistogramMetric, self).__init__(name, tags, MetricType.HISTOGRAM, max_metrics, rate)
+    def __init__(self, name, value, tags, max_metric_samples=0, rate=1.0):
+        super(HistogramMetric, self).__init__(name, tags, MetricType.HISTOGRAM, max_metric_samples, rate)
         self.aggregate(value)
 
 
 class DistributionMetric(BufferedMetric):
-    def __init__(self, name, value, tags, max_metrics=0, rate=1.0):
-        super(DistributionMetric, self).__init__(name, tags, MetricType.DISTRIBUTION, max_metrics, rate)
+    def __init__(self, name, value, tags, max_metric_samples=0, rate=1.0):
+        super(DistributionMetric, self).__init__(name, tags, MetricType.DISTRIBUTION, max_metric_samples, rate)
         self.aggregate(value)
 
 
 class TimingMetric(BufferedMetric):
-    def __init__(self, name, value, tags, max_metrics=0, rate=1.0):
-        super(TimingMetric, self).__init__(name, tags, MetricType.TIMING, max_metrics, rate)
+    def __init__(self, name, value, tags, max_metric_samples=0, rate=1.0):
+        super(TimingMetric, self).__init__(name, tags, MetricType.TIMING, max_metric_samples, rate)
         self.aggregate(value)
--- tests/unit/dogstatsd/test_buffered_metrics.py
+++ tests/unit/dogstatsd/test_buffered_metrics.py
@@ -5,7 +5,7 @@ from datadog.dogstatsd.metric_types import MetricType
 class TestBufferedMetric(unittest.TestCase):
 
     def test_new_histogram_metric(self):
-        s = HistogramMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = HistogramMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         self.assertEqual(s.data, [1.0])
         self.assertEqual(s.name, "test")
         self.assertEqual(s.tags, "tag1,tag2")
@@ -13,7 +13,7 @@ class TestBufferedMetric(unittest.TestCase):
         self.assertEqual(s.metric_type, MetricType.HISTOGRAM)
 
     def test_histogram_metric_aggregate(self):
-        s = HistogramMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = HistogramMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         s.aggregate(123.45)
         self.assertEqual(s.data, [1.0, 123.45])
         self.assertEqual(s.name, "test")
@@ -22,7 +22,7 @@ class TestBufferedMetric(unittest.TestCase):
         self.assertEqual(s.metric_type, MetricType.HISTOGRAM)
 
     def test_flush_histogram_metric_aggregate(self):
-        s = HistogramMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = HistogramMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         m = s.flush()
         self.assertEqual(m['metric_type'], MetricType.HISTOGRAM)
         self.assertEqual(m['values'], [1.0])
@@ -39,14 +39,14 @@ class TestBufferedMetric(unittest.TestCase):
         self.assertEqual(m['tags'], "tag1,tag2")
 
     def test_new_distribution_metric(self):
-        s = DistributionMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = DistributionMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         self.assertEqual(s.data, [1.0])
         self.assertEqual(s.name, "test")
         self.assertEqual(s.tags, "tag1,tag2")
         self.assertEqual(s.metric_type, MetricType.DISTRIBUTION)
 
     def test_distribution_metric_aggregate(self):
-        s = DistributionMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = DistributionMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         s.aggregate(123.45)
         self.assertEqual(s.data, [1.0, 123.45])
         self.assertEqual(s.name, "test")
@@ -54,7 +54,7 @@ class TestBufferedMetric(unittest.TestCase):
         self.assertEqual(s.metric_type, MetricType.DISTRIBUTION)
 
     def test_flush_distribution_metric_aggregate(self):
-        s = DistributionMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = DistributionMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         m = s.flush()
         self.assertEqual(m['metric_type'], MetricType.DISTRIBUTION)
         self.assertEqual(m['values'], [1.0])
@@ -70,14 +70,14 @@ class TestBufferedMetric(unittest.TestCase):
         self.assertEqual(m['tags'], "tag1,tag2")
 
     def test_new_timing_metric(self):
-        s = TimingMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = TimingMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         self.assertEqual(s.data, [1.0])
         self.assertEqual(s.name, "test")
         self.assertEqual(s.tags, "tag1,tag2")
         self.assertEqual(s.metric_type, MetricType.TIMING)
 
     def test_timing_metric_aggregate(self):
-        s = TimingMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = TimingMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         s.aggregate(123.45)
         self.assertEqual(s.data, [1.0, 123.45])
         self.assertEqual(s.name, "test")
@@ -85,7 +85,7 @@ class TestBufferedMetric(unittest.TestCase):
         self.assertEqual(s.metric_type, MetricType.TIMING)
 
     def test_flush_timing_metric_aggregate(self):
-        s = TimingMetric(name="test", value=1.0, tags="tag1,tag2", max_metrics=0, rate=1.0)
+        s = TimingMetric(name="test", value=1.0, tags="tag1,tag2", max_metric_samples=0, rate=1.0)
         m = s.flush()
         self.assertEqual(m['metric_type'], MetricType.TIMING)
         self.assertEqual(m['values'], [1.0])
