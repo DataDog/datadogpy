4dce2a228b7c35ad52ac7c10dccce4516512753e
Lukasz Piatkowski <lukaspiatkowski@gmail.com>
2025-02-21 09:37:59 +0100

    Fix imports in __init__.py files to explicitly re-export symbols
    By default imports in __init__.py files are treated by type checkers as
    private. In order to re-export them one have to either list them in
    __all__ module-level attribute or use one of the special import forms.

--- datadog/api/__init__.py
+++ datadog/api/__init__.py
@@ -24,29 +24,31 @@ _mute = True
 _return_raw_response = False
 
 # Resources
-from datadog.api.comments import Comment
-from datadog.api.dashboard_lists import DashboardList
-from datadog.api.distributions import Distribution
-from datadog.api.downtimes import Downtime
-from datadog.api.timeboards import Timeboard
-from datadog.api.dashboards import Dashboard
-from datadog.api.events import Event
-from datadog.api.infrastructure import Infrastructure
-from datadog.api.metadata import Metadata
-from datadog.api.metrics import Metric
-from datadog.api.monitors import Monitor
-from datadog.api.screenboards import Screenboard
-from datadog.api.graphs import Graph, Embed
-from datadog.api.hosts import Host, Hosts
-from datadog.api.service_checks import ServiceCheck
-from datadog.api.tags import Tag
-from datadog.api.users import User
-from datadog.api.aws_integration import AwsIntegration
-from datadog.api.aws_log_integration import AwsLogsIntegration
-from datadog.api.azure_integration import AzureIntegration
-from datadog.api.gcp_integration import GcpIntegration
-from datadog.api.roles import Roles
-from datadog.api.permissions import Permissions
-from datadog.api.service_level_objectives import ServiceLevelObjective
-from datadog.api.synthetics import Synthetics
-from datadog.api.logs import Logs
+from datadog.api.comments import Comment as Comment
+from datadog.api.dashboard_lists import DashboardList as DashboardList
+from datadog.api.distributions import Distribution as Distribution
+from datadog.api.downtimes import Downtime as Downtime
+from datadog.api.timeboards import Timeboard as Timeboard
+from datadog.api.dashboards import Dashboard as Dashboard
+from datadog.api.events import Event as Event
+from datadog.api.infrastructure import Infrastructure as Infrastructure
+from datadog.api.metadata import Metadata as Metadata
+from datadog.api.metrics import Metric as Metric
+from datadog.api.monitors import Monitor as Monitor
+from datadog.api.screenboards import Screenboard as Screenboard
+from datadog.api.graphs import Graph as Graph, Embed as Embed
+from datadog.api.hosts import Host as Host, Hosts as Hosts
+from datadog.api.service_checks import ServiceCheck as ServiceCheck
+from datadog.api.tags import Tag as Tag
+from datadog.api.users import User as User
+from datadog.api.aws_integration import AwsIntegration as AwsIntegration
+from datadog.api.aws_log_integration import AwsLogsIntegration as AwsLogsIntegration
+from datadog.api.azure_integration import AzureIntegration as AzureIntegration
+from datadog.api.gcp_integration import GcpIntegration as GcpIntegration
+from datadog.api.roles import Roles as Roles
+from datadog.api.permissions import Permissions as Permissions
+from datadog.api.service_level_objectives import (
+    ServiceLevelObjective as ServiceLevelObjective,
+)
+from datadog.api.synthetics import Synthetics as Synthetics
+from datadog.api.logs import Logs as Logs
